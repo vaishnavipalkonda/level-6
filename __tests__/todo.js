091e5b891fd369b6f12995fd1b9ddec9fcc31fbb
vaishnavipalkonda <vaishnavipalkonda@gmail.com>
2023-01-07 18:02:45 +0530

    x

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -64,7 +64,12 @@ describe("Todo test suite", () => {
     await login(agent, "user.a@test.com", "123456");
     const res = await agent.get("/todos");
     const csrfToken = extractCsrfToken(res);
-    
+    const response = await agent.post("/todos").send({
+      title: "Buy milk",
+      
+      completed: false,
+      _csrf: csrfToken,
+    });
     
     expect(response.statusCode).toBe(302);
   });
